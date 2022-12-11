b4de21f8e059e683887bf507d937a7355d6c0b59
Murilo Matsubara <murilo.matsubara@gmail.com>
2022-12-11 13:05:35 -0300

    Update day-5

--- day-5/src/main.rs
+++ day-5/src/main.rs
@@ -1,34 +1,87 @@
-use std::{cmp::max, fs, iter};
+use std::{cmp::max, fs, iter, time::Instant};
 
 use regex::Regex;
 
-fn main() {
-    let (stacks, instructions) = parse_input();
+fn part_one(input: &[String]) -> String {
+    let (mut stacks, instructions) = parse_stacks_and_instructions(input);
 
-    let mut part_one_stacks = stacks.clone();
     for instruction in &instructions {
-        instruction.apply_part_one(&mut part_one_stacks);
+        instruction.apply_part_one(&mut stacks);
     }
 
-    let mut part_two_stacks = stacks;
-    for instruction in &instructions {
-        instruction.apply_part_two(&mut part_two_stacks);
-    }
-
-    let part_one: String = part_one_stacks
+    stacks
         .iter()
         .filter_map(|stack| stack.last())
         .cloned()
-        .collect();
+        .collect()
+}
 
-    let part_two: String = part_two_stacks
+fn part_two(input: &[String]) -> String {
+    let (mut stacks, instructions) = parse_stacks_and_instructions(input);
+
+    for instruction in &instructions {
+        instruction.apply_part_two(&mut stacks);
+    }
+
+    stacks
         .iter()
         .filter_map(|stack| stack.last())
         .cloned()
+        .collect()
+}
+
+fn parse_stacks_and_instructions(input: &[String]) -> (Vec<Vec<char>>, Vec<Instruction>) {
+    let Some(blank_line_idx) = input.iter().position(|line| line.is_empty()) else {
+        panic!("Invalid input: should contain a blank line separating the stacks and the instructions");
+    };
+
+    if blank_line_idx == 0 {
+        panic!("Invalid input: should not contain any blank before the stacks")
+    }
+
+    let stacks_label_idx = blank_line_idx - 1;
+    let stacks_label = &input[stacks_label_idx];
+    let stacks_idxs: Vec<usize> = stacks_label
+        .chars()
+        .map(|c| c as u8)
+        .enumerate()
+        .filter(|(_, c)| *c >= b'1' && *c <= b'9')
+        .map(|(i, _)| i)
         .collect();
 
-    println!("Part one: {}", part_one);
-    println!("Part two: {}", part_two);
+    let mut stacks: Vec<Vec<char>> = iter::repeat_with(Vec::new)
+        .take(stacks_idxs.len())
+        .collect();
+
+    for line in input
+        .iter()
+        .take(stacks_label_idx)
+        .rev()
+        .map(|line| line.as_bytes())
+    {
+        for (stack_idx, line_idx) in stacks_idxs.iter().enumerate() {
+            let stack_char = line[*line_idx];
+            if stack_char != b' ' {
+                stacks[stack_idx].push(char::from(stack_char));
+            }
+        }
+    }
+
+    let instruction_regex = Regex::new(r"\D*(\d+)\D*(\d+)\D*(\d+)").expect("Invalid regex");
+    let instructions: Vec<Instruction> = input
+        .iter()
+        .skip(blank_line_idx)
+        .filter_map(|line| instruction_regex.captures_iter(line).next())
+        .filter_map(|group| {
+            Some(Instruction {
+                amount: group.get(1)?.as_str().parse::<usize>().unwrap(),
+                from: group.get(2)?.as_str().parse::<usize>().unwrap() - 1,
+                to: group.get(3)?.as_str().parse::<usize>().unwrap() - 1,
+            })
+        })
+        .collect();
+
+    (stacks, instructions)
 }
 
 #[derive(Debug)]
@@ -61,60 +114,50 @@ impl Instruction {
     }
 }
 
-fn parse_input() -> (Vec<Vec<char>>, Vec<Instruction>) {
-    let input = fs::read_to_string("input.txt").expect("input.txt file not found");
+// --- TESTS ---
 
-    let lines: Vec<&str> = input.lines().collect();
+#[cfg(test)]
+mod test {
+    use super::*;
 
-    let Some(blank_line_idx) = lines.iter().position(|line| line.is_empty()) else {
-        panic!("Invalid input: should contain a blank line separating the stacks and the instructions");
-    };
-
-    if blank_line_idx == 0 {
-        panic!("Invalid input: should not contain any blank before the stacks")
+    #[test]
+    fn test_part_one() {
+        let input = parse_input(true);
+        let result = part_one(&input);
+        assert_eq!(result, "CMZ");
     }
 
-    let stacks_label_idx = blank_line_idx - 1;
-    let stacks_label = &lines[stacks_label_idx];
-    let stacks_idxs: Vec<usize> = stacks_label
-        .chars()
-        .map(|c| c as u8)
-        .enumerate()
-        .filter(|(_, c)| *c >= b'1' && *c <= b'9')
-        .map(|(i, _)| i)
-        .collect();
-
-    let mut stacks: Vec<Vec<char>> = iter::repeat_with(Vec::new)
-        .take(stacks_idxs.len())
-        .collect();
-
-    for line in lines
-        .iter()
-        .take(stacks_label_idx)
-        .rev()
-        .map(|line| line.as_bytes())
-    {
-        for (stack_idx, line_idx) in stacks_idxs.iter().enumerate() {
-            let stack_char = line[*line_idx];
-            if stack_char != b' ' {
-                stacks[stack_idx].push(char::from(stack_char));
-            }
-        }
+    #[test]
+    fn test_part_two() {
+        let input = parse_input(true);
+        let result = part_two(&input);
+        assert_eq!(result, "MCD");
     }
+}
 
-    let instruction_regex = Regex::new(r"\D*(\d+)\D*(\d+)\D*(\d+)").expect("Invalid regex");
-    let instructions: Vec<Instruction> = lines
-        .iter()
-        .skip(blank_line_idx)
-        .filter_map(|line| instruction_regex.captures_iter(line).next())
-        .filter_map(|group| {
-            Some(Instruction {
-                amount: group.get(1)?.as_str().parse::<usize>().unwrap(),
-                from: group.get(2)?.as_str().parse::<usize>().unwrap() - 1,
-                to: group.get(3)?.as_str().parse::<usize>().unwrap() - 1,
-            })
-        })
-        .collect();
+// --- Lines bellow do not need to be modified ---
 
-    (stacks, instructions)
+fn main() {
+    let input = parse_input(false);
+
+    let start_one = Instant::now();
+    let result_one = part_one(&input);
+    let elapsed_one = start_one.elapsed();
+
+    let start_two = Instant::now();
+    let result_two = part_two(&input);
+    let elapsed_two = start_two.elapsed();
+
+    println!("Part one result: {result_one} [time: {:.2?}]", elapsed_one);
+    println!("Part two result: {result_two} [time: {:.2?}]", elapsed_two);
+}
+
+fn parse_input(test: bool) -> Vec<String> {
+    let file = if test { "input.test.txt" } else { "input.txt" };
+
+    fs::read_to_string(file)
+        .unwrap_or_else(|_| panic!("'{file}' not found"))
+        .lines()
+        .map(|line| line.to_owned())
+        .collect()
 }
