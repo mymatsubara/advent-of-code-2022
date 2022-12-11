2dfe5e89adb725bdf72868297357ad18950a0b1b
Murilo Matsubara <murilo.matsubara@gmail.com>
2022-12-11 12:40:52 -0300

    Update day-3

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,22 +1,21 @@
-use std::{collections::HashSet, fs};
+use std::{collections::HashSet, fs, time::Instant};
 
-fn main() {
-    let lines = parse_input();
-
-    let part_one = lines
+fn part_one(input: &[String]) -> String {
+    input
         .iter()
         .filter_map(|line| get_rucksack_duplicate(line))
         .map(|c| to_priority(&c))
-        .sum::<usize>();
+        .sum::<usize>()
+        .to_string()
+}
 
-    let part_two = lines
+fn part_two(input: &[String]) -> String {
+    input
         .chunks(3)
         .filter_map(get_group_duplicate)
         .map(|c| to_priority(&c))
-        .sum::<usize>();
-
-    println!("Part one: {}", part_one);
-    println!("Part two: {}", part_two);
+        .sum::<usize>()
+        .to_string()
 }
 
 fn to_priority(c: &char) -> usize {
@@ -57,8 +56,50 @@ fn get_group_duplicate(rucksacks: &[String]) -> Option<char> {
     duplicates.into_iter().next()
 }
 
-fn parse_input() -> Vec<String> {
-    let input = fs::read_to_string("input.txt").expect("input.txt file not found");
+// --- TESTS ---
 
-    input.lines().map(|line| line.to_owned()).collect()
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = parse_input(true);
+        let result = part_one(&input);
+        assert_eq!(result, "157");
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = parse_input(true);
+        let result = part_two(&input);
+        assert_eq!(result, "70");
+    }
+}
+
+// --- Lines bellow do not need to be modified ---
+
+fn main() {
+    let input = parse_input(false);
+
+    let start_one = Instant::now();
+    let result_one = part_one(&input);
+    let elapsed_one = start_one.elapsed();
+
+    let start_two = Instant::now();
+    let result_two = part_two(&input);
+    let elapsed_two = start_two.elapsed();
+
+    println!("Part one result: {result_one} [time: {:.2?}]", elapsed_one);
+    println!("Part two result: {result_two} [time: {:.2?}]", elapsed_two);
+}
+
+fn parse_input(test: bool) -> Vec<String> {
+    let file = if test { "input.test.txt" } else { "input.txt" };
+
+    fs::read_to_string(file)
+        .unwrap_or_else(|_| panic!("'{file}' not found"))
+        .lines()
+        .map(|line| line.to_owned())
+        .collect()
 }
